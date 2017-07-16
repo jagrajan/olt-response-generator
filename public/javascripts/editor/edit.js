ba1bfe0d345d0089221a4391d5e4b47c783f78e5
jaggers <jaggers@localhost.localdomain>
2017-07-16 15:59:05 -0700

    Added deletion from edit page

--- public/javascripts/editor/edit.js
+++ public/javascripts/editor/edit.js
@@ -1,3 +1,7 @@
-var simplemde = new SimpleMDE({ element: $("#main-editor")[0] });
-
-$('.ui.checkbox').checkbox();
+$(document).ready(function() {
+    var simplemde = new SimpleMDE({ element: $("#main-editor")[0] });
+    $('.ui.checkbox').checkbox();
+    $('.delete-snippet-modal-show').click(function() {
+        $('.ui.basic.modal').modal('show');
+    });
+});
